e2a511eff5f1587e5f9a936997c8b80b1a730e6c
Jose Maria Zamora Perez <jzamora@biada.net>
2021-05-27 00:11:13 +0200

    arreglo de arrow function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const palabra = "hola buenos dias que tal";
+const mayus = palabra.charAt(0).toUpperCase() + palabra.slice(1);
+console.log(mayus);
 function upperCase() {
     let separar = palabra.split(" ")
     separar = separar.map(
-        function (n) {
-            return n.charAt(0).toUpperCase() + n.slice(1);
-        })
-    separar = separar.join(" ").toString();
+        (n) => 
+           n.charAt(0).toUpperCase() + n.slice(1)
+        ) 
+    separar = separar.join(" ");
     console.log(separar)
 
 }
